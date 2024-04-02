bbd98a1358472251c043b779a6db7e8b365eaa30
Andres Garcia Larrosa <andriusmw@yahoo.es>
2024-04-02 12:52:18 +0200

    mat input + mat select + matdatepicker

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,23 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatMomentDateModule} from '@angular/material-moment-adapter';
+import { MatDatepickerModule } from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule , MatSlideToggleModule],
+  imports: [RouterOutlet, 
+    CommonModule , 
+    MatSlideToggleModule, 
+    MatInputModule, 
+    MatSelectModule, 
+    MatDatepickerModule,
+    MatMomentDateModule,
+  
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
