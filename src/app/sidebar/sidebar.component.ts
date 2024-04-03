8a5676faac75c87e7b2d041629dbf505d32cf90c
Andres Garcia Larrosa <andriusmw@yahoo.es>
2024-04-03 11:26:28 +0200

    sidenavbar working

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -2,13 +2,20 @@ import { Component } from '@angular/core';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar'
 import { RouterOutlet } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button'
+import {MatIconModule} from '@angular/material/icon'
+
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
   imports: [    MatSidenavModule,
     MatToolbarModule,
-    RouterOutlet],
+    RouterOutlet,
+    MatButtonModule,
+    MatIconModule
+
+  ],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
